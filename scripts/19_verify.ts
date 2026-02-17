a61350f24e4910adac56eeb475baad1a49bee21d
Andrey Kobinyak <wanted635@gmail.com>
2026-02-17 10:15:15 +0300

    add delay before contract verification

--- scripts/19_verify.ts
+++ scripts/19_verify.ts
@@ -2,8 +2,10 @@ const hre = require("hardhat");
 
 export async function verify(address: string, args: any) {
   try {
-    return hre.run("verify:verify", {address: address, constructorArguments: args,});
-  } catch(e) {
+    console.log("Waiting for 2 minutes before verification...");
+    await new Promise((resolve) => setTimeout(resolve, 120000));
+    return hre.run("verify:verify", { address: address, constructorArguments: args, });
+  } catch (e) {
     console.log(e);
   }
 }
